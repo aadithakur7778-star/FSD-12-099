db76fdf800500a90e48651a7889dad0aa6d13a73
Aditya Singh <aadithakur7778@gmail.com>
2026-07-23 11:54:05 +0530

    commit

---
+++ LAB-1/hello.js
@@ -0,0 +1,26 @@
+console.log("hello dhanno sir Aditya this side")
+
+//arithematic function for two number
+
+function sum(a, b) {
+    return a + b;
+};
+console.log(sum(5, 10));
+
+const add=(a, b) => a + b;
+console.log(add(5, 10)); 
+
+const sub=(a, b) => a - b;
+console.log(sub(10, 5));  
+
+const mul=(a, b) => a * b;  
+console.log(mul(5, 10));
+
+const div=(a, b) => a / b;
+console.log(div(10, 5));
+
+const mod=(a, b) => a % b;
+console.log(mod(10, 3));
+
+const pow=(a, b) => a ** b;
+console.log(pow(2, 3));
